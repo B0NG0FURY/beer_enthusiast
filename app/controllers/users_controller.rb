8be0df65bc2a8d45ce5d92a7bfc65c2fbb0c8c2c
bongofury <jtroutmanman@gmail.com>
2021-04-09 02:06:10 -0500

    added code to user#create action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,21 @@ class UsersController < ApplicationController
     end
 
     def create
+        user = User.create(user_params)
+        if user.save
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            redirect_to signup_path
+        end
     end
 
     def show
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
     end
 end
