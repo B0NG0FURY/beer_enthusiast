1ea7646bb8646e5226f362d77d00776d9eacbe58
bongofury <jtroutmanman@gmail.com>
2021-04-14 20:41:15 -0500

    created breweries#index method in breweries controller. created view for breweries#index with code to iterate over array depending on if viewing a user's list or all breweries. created sort_breweries_by_name helper.

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,2 +1,10 @@
 class BreweriesController < ApplicationController
+    def index
+        if params[:user_id]
+            @user = User.find_by_id(params[:user_id])
+            @breweries = @user.breweries
+        else
+            @breweries = Brewery.all
+        end
+    end
 end
--- app/helpers/breweries_helper.rb
+++ app/helpers/breweries_helper.rb
@@ -1,2 +1,5 @@
 module BreweriesHelper
+    def sort_breweries_by_name(array)
+        array.sort_by {|brewery| brewery.name}.uniq
+    end
 end
