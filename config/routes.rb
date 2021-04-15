3e51a057ee024e5308efb297fd49b55e77eab568
bongofury <jtroutmanman@gmail.com>
2021-04-14 20:46:48 -0500

    added nested route for breweries index and show pages through users.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :create] do
     resources :beers, only: [:index, :show]
     resources :reviews, only: [:index, :edit]
+    resources :breweries, only: [:index, :show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
