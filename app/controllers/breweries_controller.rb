9bbcac0b20a1847b9c899dee5ba135247167afad
bongofury <jtroutmanman@gmail.com>
2021-04-16 14:52:40 -0500

    created breweries#show in breweries controller with logic to change which beers are shown depending on if viewing all a breweries beers or just the beers a user has tried if viewing through their nested user path. added edge case to beer partial so brewery name will not repeat if viewing beers through brewery show page.

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -7,4 +7,14 @@ class BreweriesController < ApplicationController
             @breweries = Brewery.all
         end
     end
+
+    def show
+        @brewery = Brewery.find_by_id(params[:id])
+        if params[:user_id]
+            @user = User.find_by_id(params[:user_id])
+            @beers = @user.beers.where(["brewery_id = ?", @brewery.id])
+        else
+            @beers = @brewery.beers
+        end
+    end
 end
