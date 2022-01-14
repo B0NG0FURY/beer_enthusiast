b610af494605afe4488c63536dfb24f9ca24f5ea
bongofury <jtroutmanman@gmail.com>
2022-01-14 16:17:53 -0600

    breweries index is now paginated.

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -2,10 +2,10 @@ class BreweriesController < ApplicationController
     def index
         if params[:user_id]
             @user = User.find_by_id(params[:user_id])
-            @breweries = @user.breweries
+            @breweries = @user.breweries.order(:name).page params[:page]
             redirect_to breweries_path if !logged_in? || current_user.id != @user.id
         else
-            @breweries = Brewery.all
+            @breweries = Brewery.all.order(:name).page params[:page]
         end
     end
 
