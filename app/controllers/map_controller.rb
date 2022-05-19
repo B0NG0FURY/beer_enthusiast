2da34f6284885c9556fdb523fca3ca15b975c722
bongofury <jtroutmanman@gmail.com>
2022-05-18 20:06:31 -0500

    reworded brewery locator form select element option. wrote code to snag breweries from api and return results in map#results.

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,5 +1,14 @@
 class MapController < ApplicationController
     def results
-        @location = params[:location]
+        api = "https://api.openbrewerydb.org/breweries"
+        @location = params[:location].downcase
+        
+        if params[:type] == "City"
+            @breweries = Excon.get("#{api}?by_city=#{@location}")
+            puts @breweries.body
+        else
+            @breweries = Excon.get("#{api}?by_postal=#{@location}")
+            puts @breweries.body
+        end
     end
 end
