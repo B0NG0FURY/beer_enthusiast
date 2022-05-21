de00eacd9fd4d92f11a1472a0dcd6dff84316d97
bongofury <jtroutmanman@gmail.com>
2022-05-21 17:04:57 -0500

    brewery locator search results are not titleized and presented correctly on view page. DRYed code in map#results.

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,16 +2,17 @@ class MapController < ApplicationController
     def results
         api = "https://api.openbrewerydb.org/breweries"
         if params[:location]
-            @location = params[:location].downcase
+            @location = params[:location]
         end
+
+        searchable_location = @location.downcase.split(" ").join("_")
         
         if params[:type] == "City"
-            response = Excon.new("#{api}?by_city=#{@location}")
-            response = response.get(:headers => {'Accept' => 'application/json'})
+            response = Excon.new("#{api}?by_city=#{searchable_location}")
         else
-            response = Excon.new("#{api}?by_postal=#{@location}")
-            response = response.get(:headers => {'Accept' => 'application/json'})
+            response = Excon.new("#{api}?by_postal=#{searchable_location}")
         end
+        response = response.get(:headers => {'Accept' => 'application/json'})
         @breweries = JSON.parse(response.body)
     end
 end
